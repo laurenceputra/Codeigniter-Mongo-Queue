bde9f40ebb66cb01fca82500d4be8b3353ed72f3
laurenceputra <laurenceputra@gmail.com>
2013-03-01 16:52:18 +0800

    updated core_model

--- application/models/core_model.php
+++ application/models/core_model.php
@@ -3,10 +3,10 @@
 class Core_model extends CI_Model {
     public function connect_to_db(){
         $options = array('db' => MONGO_DB_NAME,
-            'replicaSet' => 'laurenceputra1',
+            'replicaSet' => MONGO_REPLICA_SET,
             'readPreference' => MongoClient::RP_SECONDARY_PREFERRED);
-        $conn = new MongoClient('mongodb://'.MONGO_USERNAME.':'.MONGO_PASSWORD.'@216.12.210.216:19904,216.12.199.124:22380', $options);
+        $conn = new MongoClient($mongo_uri, $options);
         // $conn = new Mongo();
-        return $conn->instasyncer;
+        return $conn->database;
     }
 }
